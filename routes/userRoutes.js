826240ce08c36a78da758a336f62fbfb8c22ba08
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2023-09-18 18:18:31 -0400

    ready to host2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 //router.post("/sendAdminNotif", mailController.sendAdminNotif);
-router.get("/test", (req, res) => res.json({ msg: "users work!" }));
+router.get("/test", (req, res) => res.json({ msg: "users work2!" }));
 router.get("/getCreaToken", userController.getCreaToken);
 router.post("/getProperties", userController.getProperties);
 router.post("/getPropertyByKey", userController.getPropertyByKey);
